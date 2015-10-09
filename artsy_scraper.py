d4a4d95d1753dcaa19467cabcf568d71b5213677
olgageletina <olgaguillotine@gmail.com>
2015-10-09 17:58:06 -0400

    more changes

--- artsy_scraper.py
+++ artsy_scraper.py
@@ -12,6 +12,11 @@ from bs4 import BeautifulSoup
 import StringIO
 import html5lib
 import getpass
+import math
+
+# maximun number of records per page
+
+RECORDS_PER_PAGE = 100
 
 # define API URL
 
@@ -33,9 +38,15 @@ DEFAULT_CATEGORIES = ['painting'
 
 # scrape!
 class UnauthorizedException(Exception):
+    """
+    This exception is raised when there is a bad unauthorized response.
+    """
     pass
 
 class HTTPException(Exception):
+    """
+    This exception is raised when there is a bad response that's other than unauthorized.
+    """
     pass
 
 class ArtsyScraper(object):
@@ -46,30 +57,38 @@ class ArtsyScraper(object):
         self.token = token
 
     def get_artworks_by_categories(self, categories = DEFAULT_CATEGORIES, max_results_per_category = 10000):
-        records = []
+        """
+        This method sends a request to the artsy.net API and scrapes data based on the desired categories and number of results.
+        """
+        records = [] 
+
+        pages = max_results_per_category / RECORDS_PER_PAGE
+        if max_results_per_category % RECORDS_PER_PAGE > 0:
+            pages = pages +1 
+        else pages
+        
         for category in categories:
-            for page in xrange(1, max_results_per_category / 100):
+            for page in xrange(1, pages + 1):
                 session = requests.session()
                 session.headers['X-XAPP-TOKEN'] = self.token
                 response = session.get(CATEGORY_API_URL.format(page = page, category = category))
 
                 if response.status_code == requests.codes.unauthorized:
                     raise UnauthorizedException(response.json()['text'])
-
-                if not response.ok:
-                    #print records
-                    print response.json()
+                elif response.status_code == requests.codes.not_found:
                     break
-                    #raise HTTPException(response.json())
+                elif not response.ok:
+                    raise HTTPException(response.json())
 
-                records.append(response.json())
-                #break
+                records.extend(response.json()[0:(max_results_per_category - len(records))])
 
         return records
-        #return pd.concat([pd.DataFrame(rec) for rec in records])
+        # int(math.ceil(1.0 * max_results_per_category / 100))
+        #+ ((max_results_per_category % 100) ? 1 : 0)
 
 
-new_token = 'JvTPWe4WsQO-xqX6Bts49jf1k5gBrB8xKt4tShB-pfx4_xIuvOwOVBafP5_PgGZh3qfN8V508TmWgs8XWNevka00wQmdqxfcWqQKcchA6UrdFtWkT1L--u5dV5M2Y5SISuCdZcBIT7qn6by-Rtz2msCm9o7J_z85TaP2m-khd9-CsGBbhQX0pakBKDDGU022gpKD6tHK4YTnKf6LrzzS_xl_mCRp-5vFOPylWdUArGg='
+new_token = 'JvTPWe4WsQO-xqX6Bts49hlE3XSiQBM830BTU2YBHhP8i2ac3_mgIeJcYmO5l_Q0liaI-Nk9K7Jld8FxBtCj5Q62BGCWCN0s29VIpAfj1xhuMHoBd5NoOeXVYlKy4PGsNu9-I5DYj4J6STO53yscSu5yMe_tQtCmJqF3Lipq-eTk_JX9eEfZgL7tYxzLfZ83NmFm9jMATEOI-_xKNpg2NQzW539tpuKFfIu9jbcp-2o='
 test = ArtsyScraper(new_token)
 
-test.get_artworks_by_categories(categories = DEFAULT_CATEGORIES, max_results_per_category = 200)
+art = test.get_artworks_by_categories(categories = DEFAULT_CATEGORIES, max_results_per_category = 200)
+print art
