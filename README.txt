548e0f7ebcefdf74c3c0a6dabf16edd315dc2f76
olgageletina <olga@OGs-MacBook-Air.fios-router.home>
2015-11-10 08:23:58 -0500

    Documentation and templating

--- README.txt
+++
@@ -1,1 +0,0 @@
-This is a final project for a Data Science course I took at General Assembly in Winter of 2015. The two components are: the scraper and the model. Stay tuned.
--- artsy_scraper.py
+++ artsy_scraper.py
@@ -1,10 +1,10 @@
 """
 .. module:: artsy_scraper.py
-   :synopsis: This scraper is designed to scrape the information on artworks available on artsy.net. This script returns JSON records.
+   :synopsis: This scraper is designed to attain information on artworks featured on artsy.net. This script returns JSON records.
 
-.. moduleauthor:: Olga Geletina (under tutelage of Jon Kaczynski)
+.. moduleauthor:: Olga Geletina
 
-Docstring for my module
+Doc string for my module
 
 .. data:: RECORDS_PER_PAGE
     
@@ -12,14 +12,11 @@ Docstring for my module
 
 .. data:: CATEGORY_API_URL
 
-    The API URL used as input for get_artworks_by_categories to scrape artworks
+    The API URL used as input for get_artworks_by_categories to scrape artwork information
     
 .. data:: DEFAULT_CATEGORIES
     
-    Enumerates various artwork categories, used as input for get_artworks_by_categories to scrape artworks 
-
-more specifically, the browse home page: 'https://www.artsy.net/browse'. 
-compared to the information attained using the artsy API, the scraper results are more numerous -- the resulting dataset ranges between 60,000 - 70,000 records.
+    Enumerates various artwork categories, used as input for get_artworks_by_categories to scrape artwork information
 """
 
 import re
@@ -58,7 +55,7 @@ class UnauthorizedException(Exception):
 class HTTPException(Exception):
     """
     .. class:: HTTPException
-        This exception is raised when there is a bad response that's other than unauthorized.
+        This exception is raised when there is a bad response that's other than unauthorized and not found.
 
     """
     pass
@@ -91,7 +88,7 @@ class ArtsyScraper(object):
 
     def calculate_pages(self, num_results):
         """
-        This method calculates the number of pages need to retrieve the of number results needed.
+        This method calculates the number of pages needed to retrieve the of number desired results.
         
         :param num_results: total number of results to be returned.
         :type num_results: int.
@@ -134,13 +131,10 @@ class ArtsyScraper(object):
                 records.extend(response.json()[0:(max_results_per_category - len(records))])
 
         return records
-        # int(math.ceil(1.0 * max_results_per_category / 100))
-        #+ ((max_results_per_category % 100) ? 1 : 0)
 
-
-#TODO: if __name__ == '__main__':
-new_token = 'JvTPWe4WsQO-xqX6Bts49n14lPUtpcfyx0tYwY0RmR_WHGS9UXT9ioTrGP348dTkHkvzkKYYMFIGvaC5my04IcIxUPz0x729rnT7LcQfjF_up7IIQPhTt_4zI0gVerPf3LXVtRrh3l12Ob8EqJ6HQ2MJIUe9SMqqqgK4Mg40NCk9lC82BHrqtOfiZvyEw6XAFZ-x2F7nuJmS07ErJRNwb9IeV7vtPJSLe5kPWOtcVmM='
-test = ArtsyScraper(new_token)
-art =  test.get_artworks_by_categories(categories = DEFAULT_CATEGORIES, max_results_per_category = 200)
-print art
-#
+if __name__ == '__main__':
+    # get a valid X-APP token 
+    new_token = 'JvTPWe4WsQO-xqX6Bts49n14lPUtpcfyx0tYwY0RmR_WHGS9UXT9ioTrGP348dTkHkvzkKYYMFIGvaC5my04IcIxUPz0x729rnT7LcQfjF_up7IIQPhTt_4zI0gVerPf3LXVtRrh3l12Ob8EqJ6HQ2MJIUe9SMqqqgK4Mg40NCk9lC82BHrqtOfiZvyEw6XAFZ-x2F7nuJmS07ErJRNwb9IeV7vtPJSLe5kPWOtcVmM='
+    test = ArtsyScraper(new_token)
+    art =  test.get_artworks_by_categories(categories = DEFAULT_CATEGORIES, max_results_per_category = 200)
+    print art
